56bec1c29436ebc3b1a3a923a23809c056fe0728
paulforrs <paulforros@gmail.com>
2023-03-11 12:10:02 +0800

    polishs todo Container, turns off API calls to avoid consuming API calls

--- index.js
+++ index.js
@@ -30,50 +30,47 @@ function checkTime(time){
 }
 
 // Qoutes
-let quotes ={
-    url: 'https://zenquotes.io/api/today',
-    fetchQuotes: function(){
-        fetch(
-           'https://api.quotable.io/quotes?tags=inspirational'
-        )
-        .then(res => res.json())
-        .then(data =>{
-            this.parseData(data)
-        })
-    },
-    parseData : function(data){
-        const quotesArr = []
-        data.results.forEach( quotes =>{
-            quotesArr.push({'author':quotes.author ,content: quotes.content})
-        })
-        displayQuotes(quotesArr)
-    }
-}
+// let quotes ={
+//     url: 'https://zenquotes.io/api/today',
+//     fetchQuotes: function(){
+//         fetch(
+//            'https://api.quotable.io/quotes?tags=inspirational'
+//         )
+//         .then(res => res.json())
+//         .then(data =>{
+//             this.parseData(data)
+//         })
+//     },
+//     parseData : function(data){
+//         const quotesArr = []
+//         data.results.forEach( quotes =>{
+//             quotesArr.push({'author':quotes.author ,content: quotes.content})
+//         })
+//         displayQuotes(quotesArr)
+//     }
+// }
 function displayQuotes(quotesArr) {
-    const index = Math.floor(Math.random()*quotesArr.length+1)
+    // const index = Math.floor(Math.random()*quotesArr.length+1)
     const quotes = document.querySelector('#quotes')
-    const quotesContent = quotesArr.splice(index, 1)
-    quotes.textContent = quotesContent[0].content
-
+    // const quotesContent = quotesArr.splice(index, 1)
+    quotes.textContent = '"'+quotesArr[0].quote +'"'
 }
 
-var category = 'inspirational'
-$.ajax({
-    method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/quotes?category=' + category+'&limit?=10',
-    headers: { 'X-Api-Key': 'DSd6FPovthYiP8npib9Afg==qO4KqQ9W6uxnpcu9'},
-    contentType: 'application/json',
-    success: function(result) {
-        console.log(result);
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
-});
-
-// setInterval(()=>{
-//     displayQuotes(quotes.parseData.quotesArr)
-// }, 60000)
+var category = 'success'
+function fetchQuotes(){
+    $.ajax({
+        method: 'GET',
+        url: 'https://api.api-ninjas.com/v1/quotes?category=' + category,
+        headers: { 'X-Api-Key': 'DSd6FPovthYiP8npib9Afg==qO4KqQ9W6uxnpcu9'},
+        contentType: 'application/json',
+        success: function(result) {
+            displayQuotes(result)
+        },
+        error: function ajaxError(jqXHR) {
+            console.error('Error: ', jqXHR.responseText);
+        }
+    });
+}
 
 
 // Weather
@@ -147,26 +144,45 @@ const todoSidebar = document.querySelector('#todo-sidebar')
 const todoContainer = document.querySelector('#todo-container')
 const todoArrow = document.querySelector('#todo-sidebar span')
 const todoListArr = Array.from(document.querySelectorAll('#todo-list >*'))
+const addTodoButton = document.querySelector('#add-todo-button');
+const addTodoInput = document.querySelector('#add-todo-input');
+const submitTodo = document.querySelector('#submit-todo')
+
 todoSidebar.addEventListener('click', ()=>{
     todoContainerToggle()
-
+    toggleTodoArrow()
 })
-
+submitTodo.addEventListener('click',()=>{
+    toggleTodoInput()
+})
+addTodoButton.addEventListener('click',()=>{
+    toggleTodoInput()
+})
 window.onclick= (e)=>{
-    console.log(e.target.parentElement.classList.contains('todo'))
-    if(!e.target.parentElement.classList.contains('todo')){
-        todoContainerToggle()
+    if(!e.target.parentElement.classList.contains('todo') && todoContainer.classList.contains('active')){
+        toggleTodoArrow()
+        todoContainer.classList.remove('active')
     }
 }
 
 function todoContainerToggle(){
     todoContainer.classList.toggle('active')
+
+}
+function toggleTodoArrow(){
     if (todoArrow.textContent =='chevron_right') {
         todoArrow.textContent = 'chevron_left'
     }
     else{
         todoArrow.textContent = 'chevron_right'
     }
+}
+
+
+
+function toggleTodoInput(){
+    addTodoButton.classList.toggle('hidden')
+    addTodoInput.classList.toggle('hidden')
 }
 // function addTodoToggle(){
 
@@ -175,8 +191,6 @@ function createTodo(){
     const todoItem = document.createElement('li')
     const checkbox = document.createElement('input').setAttribute('type','checkbox')
 }
-
-
 setInterval(()=>{
     const time = new Date();
     toogleGreeting(time);
@@ -184,8 +198,13 @@ setInterval(()=>{
 
 },1000)
 
+
 const city = 'cebu'
 weather.geoCoder.fetchCoordinate(city)
-setInterval(()=>{
-    weather.geoCoder.fetchCoordinate(city)
-},10000)
+// setInterval(()=>{
+//     weather.geoCoder.fetchCoordinate(city)
+// },30000)
+// setInterval(() => {
+//     fetchQuotes()
+// }, 60000);
+// fetchQuotes()
